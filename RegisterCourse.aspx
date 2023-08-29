68ad07484d9da05c0d942709490d82111ca87f59
Rayan Rodoshe <rodo0007@algonquinlive.com>
2023-08-29 17:56:48 -0400

    added everything

---
+++ RegisterCourse.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RegisterCourse.aspx.cs" Inherits="Lab6.RegisterCourse" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Lab 6</title>
+    <link href="App_Themes/style.css" rel="stylesheet" />
+</head>
+<body>
+    <h1>Algonquin College Course Registration</h1>
+    <form runat="server">
+        <div>
+            <div>
+                Student Name: <asp:TextBox CssClass="inputField" ID="txtStudentName" runat="server"></asp:TextBox>
+                <br />
+                <div CssClass="errorLog">
+                  <asp:Label runat="server" style="color:red;" CssClass="errorLabel" ID="lblNameError"></asp:Label>
+                  <asp:Label runat="server" style="color:red;" CssClass="errorLabel" ID="lblStudentTypeError"></asp:Label>
+                  <asp:Label runat="server" style="color:red;" CssClass="errorLabel" ID="lblCourseError"></asp:Label>
+                  <asp:Label runat="server" style="color:red;" CssClass="errorLabel" ID="lblSelectionError"></asp:Label>
+                </div>
+                <br />
+                <div>
+                    <asp:RadioButtonList ID="rblStudentType" runat="server">
+                        <asp:ListItem Text="Full Time" Value="Full Time" />
+                        <asp:ListItem Text="Part Time" Value="Part Time" />
+                        <asp:ListItem Text="Co-op" Value="Co-op" />
+                    </asp:RadioButtonList>
+                </div>
+            </div>
+            <br/>
+            <asp:Label ID="lblCourseHeader" runat="server">Following courses are currently available for registration</asp:Label>
+            <div ID="divCheckBoxSection">
+                <asp:CheckBoxList ID="cbCourseSelection" runat="server">
+                    <asp:ListItem Text="Introduction to Database Systems - 4 hours/week" Value="CST8282" />
+                    <asp:ListItem Text="Web Programming II - 2 hours/week" Value="CST8253" />
+                    <asp:ListItem Text="Web Programming Language I - 5 hours/week" Value="CST8256" />
+                    <asp:ListItem Text="Web Imaging and Animations - 2 hours/week" Value="CST8255" />
+                    <asp:ListItem Text="Network Operating System - 1 hours/week" Value="CST8254" />
+                    <asp:ListItem Text="Data Warehouse Design - 3 hours/week" Value="CST2200" />
+                    <asp:ListItem Text="Advance Database topics - 1 hours/week" Value="CST2240" />
+                </asp:CheckBoxList>
+            </div>
+            <br/>
+            <asp:PlaceHolder ID="phTable" runat="server"></asp:PlaceHolder>
+            <asp:Label runat="server" CssClass="Message" ID="lblMessage"></asp:Label>
+            <br />
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+</body>
+</html>
---
+++ RegisterCourse.aspx.cs
@@ -0,0 +1,223 @@
+﻿using Lab6.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab6
+{
+    public partial class RegisterCourse : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string studentName = "";
+            string studentType = "";
+            int totalCourseHours = 0;
+            int selectedCourseCount = 0;
+            string[] selectedCourseCodes = new string[8];
+
+            if (IsPostBack)
+            {
+                lblNameError.Visible = false;
+                lblStudentTypeError.Visible = false;
+                lblCourseError.Visible = false;
+                lblMessage.Visible = false;
+                lblSelectionError.Visible = false;
+
+                lblNameError.Text = "";
+                lblStudentTypeError.Text = "";
+                lblCourseError.Text = "";
+                lblMessage.Text = "";
+
+                if (txtStudentName.Text == "" || string.IsNullOrEmpty(txtStudentName.Text))
+                {
+                    lblNameError.Visible = true;
+                    lblNameError.Text = "You must enter a name!" + "<br/>";
+                }
+                else
+                {
+                    studentName = txtStudentName.Text;
+                }
+
+                bool isStudentTypeSelected = false;
+
+                foreach (ListItem studentTypeItem in rblStudentType.Items)
+                {
+                    if (studentTypeItem.Selected)
+                    {
+                        studentType = studentTypeItem.Value;
+                        isStudentTypeSelected = true;
+                        break;
+                    }
+                }
+
+                if (!isStudentTypeSelected)
+                {
+                    lblStudentTypeError.Text = "Please select student type!" + "<br/>";
+                    lblStudentTypeError.Visible = true;
+                    studentType = "";
+                }
+
+                bool isCourseSelected = false;
+
+                foreach (ListItem courseItem in cbCourseSelection.Items)
+                {
+                    if (courseItem.Selected)
+                    {
+                        isCourseSelected = true;
+                        break;
+                    }
+                }
+
+                if (!isCourseSelected)
+                {
+                    lblCourseError.Text = "Please select at least one course!" + "<br/>";
+                    lblCourseError.Visible = true;
+                }
+                else
+                {
+                    foreach (ListItem courseItem in cbCourseSelection.Items)
+                    {
+                        if (courseItem.Selected)
+                        {
+                            selectedCourseCodes[selectedCourseCount] = courseItem.Value;
+                            selectedCourseCount += 1;
+
+                            // Calculate total course hours
+                            switch (courseItem.Text)
+                            {
+                                case "Introduction to Database Systems - 4 hours/week":
+                                    totalCourseHours += 4;
+                                    break;
+                                case "Web Programming II - 2 hours/week":
+                                    totalCourseHours += 2;
+                                    break;
+                                case "Web Programming Language I - 5 hours/week":
+                                    totalCourseHours += 5;
+                                    break;
+                                case "Web Imaging and Animations - 2 hours/week":
+                                    totalCourseHours += 2;
+                                    break;
+                                case "Network Operating System - 1 hours/week":
+                                    totalCourseHours += 1;
+                                    break;
+                                case "Data Warehouse Design - 3 hours/week":
+                                    totalCourseHours += 3;
+                                    break;
+                                case "Advance Database topics - 1 hours/week":
+                                    totalCourseHours += 1;
+                                    break;
+                            }
+                        }
+                    }
+
+                    lblMessage.Visible = true;
+                    lblMessage.Text = $"Course count: {selectedCourseCount} / Total course hours: {totalCourseHours}" + "<br/>";
+                }
+
+                if (studentType == "Full Time" && totalCourseHours > 16)
+                {
+                    lblSelectionError.Text = "Full Time must not exceed 16 hours!" + "<br/>";
+                    lblSelectionError.Visible = true;
+                }
+                else if (studentType == "Part Time" && selectedCourseCount > 3)
+                {
+                    lblSelectionError.Text = "Part Time cannot exceed 3 courses" + "<br/>";
+                    lblSelectionError.Visible = true;
+                }
+                else if (studentType == "Co-op" && (selectedCourseCount > 2 || totalCourseHours > 4))
+                {
+                    lblSelectionError.Text = "Co-op cannot exceed 2 courses or 4 hours/week";
+                    lblSelectionError.Visible = true;
+                }
+
+                if (lblNameError.Text == "" &&
+                    lblStudentTypeError.Text == "" &&
+                    lblCourseError.Text == "" &&
+                    lblSelectionError.Text == "")
+                {
+                    Table mainTable = new Table();
+                    mainTable.ID = "tblMainTable";
+
+                    TableHeaderRow mainRow = new TableHeaderRow();
+                    TableHeaderCell courseCodeHeader = new TableHeaderCell();
+                    courseCodeHeader.Text = "Course Code";
+
+                    TableHeaderCell courseTitleHeader = new TableHeaderCell();
+                    courseTitleHeader.Text = "Course Title";
+
+                    TableHeaderCell weeklyHoursHeader = new TableHeaderCell();
+                    weeklyHoursHeader.Text = "Weekly Hours";
+
+                    courseCodeHeader.Attributes["style"] = "font-size: 13px;" +
+                                                           "padding: 8px;" +
+                                                           "background: #b9c9fe;" +
+                                                           "font-weight: normal;" +
+                                                           "border-top: 4px solid #aabcfe;" +
+                                                           "border-bottom: 1px solid #fff;" +
+                                                           "color: #039;";
+
+                    courseTitleHeader.Attributes["style"] = "font-size: 13px;" +
+                                                            "padding: 8px;" +
+                                                            "background: #b9c9fe;" +
+                                                            "font-weight: normal;" +
+                                                            "border-top: 4px solid #aabcfe;" +
+                                                            "border-bottom: 1px solid #fff;" +
+                                                            "color: #039;";
+
+                    weeklyHoursHeader.Attributes["style"] = "font-size: 13px;" +
+                                                             "padding: 8px;" +
+                                                             "background: #b9c9fe;" +
+                                                             "font-weight: normal;" +
+                                                             "border-top: 4px solid #aabcfe;" +
+                                                             "border-bottom: 1px solid #fff;" +
+                                                             "color: #039;";
+
+                    mainRow.Cells.Add(courseCodeHeader);
+                    mainRow.Cells.Add(courseTitleHeader);
+                    mainRow.Cells.Add(weeklyHoursHeader);
+                    mainTable.Rows.Add(mainRow);
+
+                    foreach (string courseCode in selectedCourseCodes)
+                    {
+                        Course courseInstance = Helper.GetCourseByCode(courseCode);
+                        if (courseInstance == null)
+                        {
+                            continue;
+                        }
+
+                        TableRow addedRow = new TableRow();
+                        TableCell courseCodeCell = new TableCell();
+                        courseCodeCell.Text = courseInstance.Code;
+
+                        TableCell courseTitleCell = new TableCell();
+                        courseTitleCell.Text = courseInstance.Title;
+
+                        TableCell weeklyHoursCell = new TableCell();
+                        weeklyHoursCell.Text = $"{courseInstance.WeeklyHours}";
+
+                        addedRow.Cells.Add(courseCodeCell);
+                        addedRow.Cells.Add(courseTitleCell);
+                        addedRow.Cells.Add(weeklyHoursCell);
+                        mainTable.Rows.Add(addedRow);
+                    }
+
+                    mainTable.Attributes["style"] = "font-family: 'Lucida Sans Unicode, Lucida Grande, Sans-Serif';" +
+                                                    "font-size: 12px;" +
+                                                    "margin: 45px;" +
+                                                    "width: 480px;" +
+                                                    "text-align: left;" +
+                                                    "border-collapse: collapse;";
+
+                    phTable.Controls.Add(mainTable);
+                    cbCourseSelection.Visible = false;
+                    lblCourseHeader.Text = "has enrolled in the following courses" + "<br/>";
+                }
+            }
+        }
+    }
+}
